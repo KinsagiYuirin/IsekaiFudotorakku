a496062444e1157a2e5414ca6a690071caf6a874
Poramat Poomarkard <poramat.poom@bumail.net>
2025-10-01 15:20:03 +0700

    - add Waiting Combo & Comboing Method

--- Assets/Kaede/Art/TA Works/Scripts/VFX_Control.cs
+++ Assets/Kaede/Art/TA Works/Scripts/VFX_Control.cs
@@ -6,8 +6,6 @@ using Random = UnityEngine.Random;
 
 public class VFX_Control : MonoBehaviour
 {
-    [Header("Pan Light")]
-    public Light2D[] panLights;
 
     [Header("Particle/VFX")] 
     public Material screenVFXmat;
@@ -18,41 +16,34 @@ public class VFX_Control : MonoBehaviour
     public float screenVFXviggnetteePower;
     public float screenVFXvignetteIntensity;
     
+    [Header("Light")]
+    public Light2D globlaLight;
+    public Light2D panLights;
+    public Light2D truckLights;
+    public Light2D shopLights;
+    
     [Header("Pan Light Setting")]
     public float minIntensity = 0.5f;
     public float maxIntensity = 1.5f;
     public float flickerSpeed = 0.1f;
+    
 
+    [Header("Screen Renderer Feature")]
+    public ScriptableRendererFeature ScreenRandererFeature;
+    
     private float flickerTarget;
     private float velocity;
 
     void Start()
     {
-        if (panLights.Length > 0)
-            flickerTarget = panLights[0].intensity;
+        WaitingCombo();
     }
 
     void Update()
     {
-        PanLightFlicker();
-        ScreenVFXSettings();
+        
     }
-    void PanLightFlicker()
-    {
-        if (panLights.Length == 0) return;
-
-        if (Random.value < flickerSpeed)
-            flickerTarget = Random.Range(minIntensity, maxIntensity);
-
-        foreach (var light in panLights)
-        {
-            if (light == null) continue;
-            float vel = velocity;
-            light.intensity = Mathf.SmoothDamp(light.intensity, flickerTarget, ref vel, 0.1f);
-            velocity = vel;
-        }
-    }
-
+    
     void ScreenVFXSettings()
     {
         screenVFXvoronoiPower = screenVFXmat.GetFloat("_VoronoiPower");
@@ -60,4 +51,32 @@ public class VFX_Control : MonoBehaviour
         screenVFXviggnetteePower = screenVFXmat.GetFloat("_VignettePower"); 
         screenVFXvignetteIntensity = screenVFXmat.GetFloat("_VignetteIntensity");
     }
+    
+    void PanLightFlicker()
+    {
+        float sinValue = Mathf.Sin(Time.time * flickerSpeed);
+        float normalizedValue = (sinValue + 1f) / 2f;
+        panLights.intensity = Mathf.Lerp(minIntensity, maxIntensity, normalizedValue);
+    }
+    
+    public void OnScreenRendererFeature(bool On)
+    {
+        ScreenRandererFeature.SetActive(On);
+    }
+
+    void WaitingCombo()
+    {
+        PanLightFlicker();
+        OnScreenRendererFeature(false);
+        globlaLight.intensity = 1f;
+        truckLights.intensity = 0.2f;
+        shopLights.intensity = 0.2f;
+    }
+
+    void Comboing()
+    {
+        PanLightFlicker();
+        globlaLight.intensity = 0.75f;
+    }
+    
 }
