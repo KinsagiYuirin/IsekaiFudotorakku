6137810a27ffe5affc902103d17e02f26132c07e
Kinsagi Yuirin <sengsoon02@gmail.com>
2025-11-14 00:51:26 +0700

    [Update] AI Animation

--- Assets/Kaede/Scripts/Animation/Manga/MangaSystem.cs
+++ Assets/Kaede/Scripts/Animation/Manga/MangaSystem.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 
 namespace Kaede.Scripts.Animation.Manga
 {
+    public enum MangaEffectType
+    {
+        None,
+        SpeedLines,
+        ImpactBurst,
+        ScreenShake,
+        ZoomIn,
+        ZoomOut
+    }
+    
+    public class 
+    
     public class MangaSystem : MonoBehaviour
     {
         [SerializeField] private MangaPage[] mangaPages;
--- Assets/Kaede/Scripts/Item/MangaPage.cs
+++ Assets/Kaede/Scripts/Item/MangaPage.cs
@@ -3,22 +3,11 @@ using UnityEngine.UI;
 
 namespace Kaede.Scripts.Item
 {
-    public enum MangaEffectType
-    {
-        None,
-        SpeedLines,
-        ImpactBurst,
-        ScreenShake,
-        ZoomIn,
-        ZoomOut
-    }
-    
     [System.Serializable]
     public class PageSetting
     {
         public Image pageSprite;
         public int pageNumber;
-        public MangaEffectType effectType;
     }
     
     [CreateAssetMenu(fileName = "MangaPage", menuName = "Scriptable Objects/MangaPage")]
